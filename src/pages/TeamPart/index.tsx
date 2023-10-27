5ac39ac5b6a4f491de57db56fb1c678ff218212b
jerodriguez6 <jamesrodriguez3188@gmail.com>
2023-10-27 15:44:11 -0500

    Perfiles

--- src/pages/TeamPart/index.tsx
+++ src/pages/TeamPart/index.tsx
@@ -50,73 +50,92 @@ const TeamContainer = () => {
               <TeamImg src="img/JAM.png" alt="" />
               <TeamItemName>JAMES RODRIGUEZ</TeamItemName>
               <TeamItemText><h2>CEO - CO Founder</h2></TeamItemText>
-              <TeamItemText>Blockchain and web engineer, with experience leading technology development and business teams. Certified in HyperLedger and passionate about blockchain. Business Developer.</TeamItemText>
+              <TeamItemText>BBlockchain engineer, with experience leading 
+                technological and business development teams. Certified in
+                 HyperLedger and passionate about blockchain. Business developer.
+              </TeamItemText>
             </TeamItem>
             <TeamItem key={2}>
               <TeamImg src="img/DIEGO.png" alt="" />
               <TeamItemName>DIEGO <br />ROMERO</TeamItemName>
               <TeamItemText><h2>CTO</h2></TeamItemText>
-              <TeamItemText>Software engineer, with more than 5 years of experience.
-                Experience in the development of solutions that implement blockchain using HyperLedger and knowledge in the development of smart contracts with Solidity.</TeamItemText>
+              <TeamItemText>
+                Software engineer, with more <br />
+                than 5 years of experience in the development of solutions
+                that implement blockchain and <br />
+                knowledge in the development of smart contracts with Solidity.
+              </TeamItemText>
+
             </TeamItem>
             <TeamItem key={3}>
               <TeamImg src="img/DAVID.png" alt="" />
               <TeamItemName>DAVID <br />GARCIA</TeamItemName>
               <TeamItemText><h2>Full Stack Developer</h2></TeamItemText>
-              <TeamItemText>Web developer, with high knowledge and mastery in database management and web layout, oriented to web3.</TeamItemText>
+              <TeamItemText>Web developer, with high knowledge and mastery
+                 in database <br />
+                 management and web layout,<br />
+                  oriented to web3.
+              </TeamItemText>
             </TeamItem>
             <TeamItem key={4}>
               <TeamImg src="img/lampar.png" alt="" />
               <TeamItemName>GERMAN <br />TROCONIS</TeamItemName>
               <TeamItemText><h2>Unreal Engine Game Developer</h2></TeamItemText>
-              <TeamItemText>Experienced developer in game mechanics, animations and game blueprint and metaverse programming, he has been involved in several major projects in the video game industry.</TeamItemText>
+              <TeamItemText>
+              Experienced developer game mechanics, animations and game plan and metaverse programming, involved in important video game projects.
+               </TeamItemText>
             </TeamItem>
             <TeamItem key={5}>
-              <TeamImg src="img/full.png" alt="" />
+              <TeamImg src="img/bily.png" alt="" />
               <TeamItemName>MARIANO DELL AQUILA</TeamItemName>
               <TeamItemText><h2>Blockchain Developer</h2></TeamItemText>
               <TeamItemText>Expert in blockchain technology development with more than 4 years of experience.
               </TeamItemText>
             </TeamItem>
             <TeamItem key={6}>
-              <TeamImg src="img/rudo.png" alt="" />
-              <TeamItemName>JESUS CAICEDO</TeamItemName>
+              <TeamImg src="img/migue.png" alt="" />
+              <TeamItemName>REYNEL CAICEDO</TeamItemName>
               <TeamItemText><h2>Full Stack Developer</h2></TeamItemText>
-              <TeamItemText>Expert in blockchain technology development with more than 4 years of experience.
+              <TeamItemText>Full stack developer, with <br />
+              experience and mastery in <br />
+              database and layout.
               </TeamItemText>
             </TeamItem>
             <TeamItem key={7}>
               <TeamImg src="img/fuer.png" alt="" />
               <TeamItemName>ISMAEL GARCIA</TeamItemName>
               <TeamItemText><h2>Frontend Developer</h2></TeamItemText>
-              <TeamItemText>Expert in blockchain technology development with more than 4 years of experience.
+              <TeamItemText>Expert developer in design and layout with more than 3 years using modern technology.
               </TeamItemText>
             </TeamItem>
             <TeamItem key={8}>
-              <TeamImg src="img/fachero.png" alt="" />
+              <TeamImg src="img/negro.png" alt="" />
               <TeamItemName>JEFFERSON MIRANDA</TeamItemName>
               <TeamItemText><h2>Backend Developer</h2></TeamItemText>
-              <TeamItemText>Expert in blockchain technology development with more than 4 years of experience.
+              <TeamItemText>Backend developer, with experience in web3 database integration, cybersecurity and DevOps.
               </TeamItemText>
             </TeamItem>
             <TeamItem key={9}>
               <TeamImg src="img/LAUSUA.png" alt="" />
               <TeamItemName>LAURA <br />SUA</TeamItemName>
-              <TeamItemText><h2>Director of design and marketing.</h2></TeamItemText>
-              <TeamItemText>Expert in digital marketing with more than 3 years creating brands and companies, she has helped many small businesses achieve their goal.</TeamItemText>
+              <TeamItemText><h2>Marketing Director</h2></TeamItemText>
+              <TeamItemText>Digital marketing expert with more than 3 years creating brands and companies, she has helped many businesses achieve their goals.</TeamItemText>
             </TeamItem>
             <TeamItem key={10}>
               <TeamImg src="img/abog.png" alt="" />
               <TeamItemName>RENE ANTONILEZ</TeamItemName>
-              <TeamItemText><h2>Legal Address</h2></TeamItemText>
-              <TeamItemText>Experienced and innovative lawyer. With a proven track record in achieving favorable results for clients. A strong leader who understands the complexities of the legal system and the world of blockchain technology.
+              <TeamItemText><h2>Legal Advisor</h2></TeamItemText>
+              <TeamItemText>Experienced lawyer, with<br />
+               a great track record, who <br />
+               understands the complexities of the legal system and the world of blockchain technology.
               </TeamItemText>
             </TeamItem>
             <TeamItem key={11}>
               <TeamImg src="img/amis.png" alt="" />
               <TeamItemName>ADMINISTRATIVE TEAM</TeamItemName>
-              <TeamItemText><h2>Legal Address</h2></TeamItemText>
-              <TeamItemText>Expert team in finance and human resources administration.
+              <TeamItemText><h2>Administrator</h2></TeamItemText>
+              <TeamItemText>Experienced personnel in supervising company operations, managing resources, making decisions, <br />
+              ensuring compliance and growth of the company.
               </TeamItemText>
             </TeamItem>
             
